877e1ab3a664f2c1d39f66d6e56ede4785816981
Ynpckqqv <144931661+Ynpckqqv@users.noreply.github.com>
2023-09-15 09:55:09 +0530

    Add files via upload

---
+++ Challenge 2.1.py
@@ -0,0 +1,18 @@
+class bankAccount:
+  def_int_(self,account_number,account_holder_name):
+      self._account_number=account_number
+  self._account_holder_name=account_holder_name
+self._account_balance=0
+
+def deposit(self,amount):
+  self._accouny_balance+=amount
+
+def withdraw(self,amount):
+  if self._account_balance>=amount:
+    self._account_balance-=amount
+  else:
+    print("insufficient balance")
+
+def display_balancr(self):
+  return self._account_balance
+  
